67290a8b2f6b5f6b92b8051bb240a27df8cda7fb
Noah Gasser <LE2088826@crc-lennox.qc.ca>
2023-04-14 09:17:00 -0400

    Commit to merge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,13 +14,11 @@ public class PlayerController : MonoBehaviour
     
     
     
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
