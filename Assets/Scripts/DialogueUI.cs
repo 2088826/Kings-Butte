9ca024b1f97ab740b8390da433dc34aa311abc51
Christophe <LE1207226@crc.lennox.qc.ca>
2023-05-01 06:44:01 -0400

    Added input to text functionality.

--- Assets/Scripts/DialogueUI.cs
+++ Assets/Scripts/DialogueUI.cs
@@ -4,6 +4,7 @@ using TMPro;
 
 public class DialogueUI : MonoBehaviour
 {
+    [SerializeField] private GameObject dialogueBox;
     [SerializeField] private TMP_Text textLabel;
     [SerializeField] private DialogueObject testDialogue;
     private TypeWriter typeWriter;
@@ -11,11 +12,13 @@ public class DialogueUI : MonoBehaviour
     private void Start()
     {
         typeWriter = GetComponent<TypeWriter>();
+        CloseDialogueBox();
         ShowDialogue(testDialogue);
     }
 
     public void ShowDialogue(DialogueObject dialogueObject)
     {
+        dialogueBox.SetActive(true);
         StartCoroutine(StepThroughDialogue(dialogueObject));
     }
 
@@ -24,6 +27,15 @@ public class DialogueUI : MonoBehaviour
         foreach(string dialogue in dialogueObject.Dialogue)
         {
             yield return typeWriter.Run(dialogue, textLabel);
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
         }
+
+        CloseDialogueBox();
+    }
+
+    private void CloseDialogueBox()
+    {
+        dialogueBox.SetActive(false);
+        textLabel.text=string.Empty;
     }
 }
