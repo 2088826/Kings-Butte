575335e516c541099aa1d9e227e1b16c1418bdc2
Noah Gasser <LE2088826@crc-lennox.qc.ca>
2023-05-07 11:04:00 -0400

    Added Blocked property to TileVacancy. Triggers for obstacles.

--- Assets/Scripts/TileVacancy.cs
+++ Assets/Scripts/TileVacancy.cs
@@ -6,20 +6,42 @@ public class TileVacancy : MonoBehaviour
 {
 
     private bool occupied;
+    private bool blocked;
 
     public bool Occupied
     {
         get { return occupied; }
     }
 
+    public bool Blocked
+    {
+        get { return blocked; }
+    }
+
+    private void Block()
+    {
+        blocked = true;
+        occupied = true;
+    }
+
+    private void Unblock()
+    {
+        blocked = false;
+        occupied = false;
+    }
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.transform.parent.tag == "Player" || other.gameObject.transform.parent.tag == "Obstacle")
+        if (other.gameObject.transform.parent.tag == "Player")
         {
             occupied = true;
 
             //Debug.Log(gameObject.name + " OCCUPIED");
+        } 
+        else if (other.gameObject.transform.parent.tag == "Obstacle")
+        {
+            Block();
         }
     }
 
@@ -31,6 +53,10 @@ public class TileVacancy : MonoBehaviour
             occupied = false;
 
             //Debug.Log(gameObject.name + " UNOCCUPIED");
+        }
+        else if (other.gameObject.transform.parent.tag == "Obstacle")
+        {
+            Unblock();
         }
     }
 }
